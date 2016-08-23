9b783e36355c147d810022ff4bd5a2c86cdd57b1
Mann <chanmannlim@gmail.com>
2016-08-23 18:33:42 -0400

    check for head and tail of linkedlist

--- Program.java
+++ Program.java
@@ -2,6 +2,8 @@ public class Program {
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
         assert list.size() == 0 : "Expected 0 but was " + list.size();
+        assert list.getHead() == null : "Expecte null but was " + list.getHead();
+        assert list.getTail() == null : "Expecte null but was " + list.getTail();
         
         // All tests are passed!
         System.out.println("All tests are passed!");
@@ -9,7 +11,15 @@ public class Program {
 }
 
 // LinkedList class
-class LinkedList {
+class LinkedList {    
+    public Object getHead() {
+        return null;
+    }
+    
+    public Object getTail() {
+        return null;
+    }
+    
     public int size() {
         return 0;
     }
