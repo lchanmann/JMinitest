3a91e9fd4b30ae384b05b521f920c20e3369ea9d
Mann <chanmannlim@gmail.com>
2016-08-23 18:09:04 -0400

    first failing test

---
+++ Program.java
@@ -0,0 +1,9 @@
+public class Program {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        assert list.size() == 0 : "Expected 0 but was " + list.size();
+        
+        // All tests are passed!
+        System.out.println("All tests are passed!");
+    }
+}
