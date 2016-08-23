86c9e61be3448ad00975bb7adb1c69c97de5ea48
Mann <chanmannlim@gmail.com>
2016-08-23 19:20:06 -0400

    run all test methods implicitly

--- LinkedListTest.java
+++ LinkedListTest.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.*;
+
 public class LinkedListTest {
     
     private LinkedList list;
@@ -5,15 +7,29 @@ public class LinkedListTest {
         list = new LinkedList();
     }
     
+    public void runTests() throws Throwable {
+        Method[] methods = getClass().getMethods();
+        for(Method method : methods) {
+            if (method.getName().startsWith("test_")) {
+                try {
+                    // call setup for each test methods
+                    setup();
+                    method.invoke(this);
+                } catch (InvocationTargetException e) {
+                    // re-throw exception in method call
+                    throw e.getCause();
+                }
+            }
+        }
+    }
+    
     public void test_initial_linkedlist() {
-        setup();
         assert list.size() == 0 : "Expected 0 but was " + list.size();
         assert list.getHead() == null : "Expecte null but was " + list.getHead();
         assert list.getTail() == null : "Expecte null but was " + list.getTail();
     }
     
     public void test_linkedlist_add() {
-        setup();
         list.add(1); list.add(2); list.add(3);
         assert list.size() == 3 : "Expected 3 but was " + list.size();
         assert list.getHead().value == 1 : "Expecte 1 but was " + list.getHead().value;
--- Program.java
+++ Program.java
@@ -1,8 +1,6 @@
 public class Program {
-    public static void main(String[] args) {
-        LinkedListTest linkedListTest = new LinkedListTest();
-        linkedListTest.test_initial_linkedlist();
-        linkedListTest.test_linkedlist_add();
+    public static void main(String[] args) throws Throwable {
+        new LinkedListTest().runTests();
 
         // All tests are passed!
         System.out.println("All tests are passed!");
